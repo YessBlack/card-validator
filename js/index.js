e3b3f81b713f015550c3798fc99c5aceb99c27a0
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2022-06-27 22:18:58 -0500

    avance proyecto

--- js/index.js
+++ js/index.js
@@ -1,8 +1,38 @@
 import { algoritmoLuhn } from "./validarTarjeta.js";
+import { validarCampos } from "./validarInformacion.js";
+const $form = document.getElementById('form')
+const $inputs = document.querySelectorAll('#form input')
+
+const validarFormulario = (e) => {
+    switch(e.target.name) {
+        case 'name-user':
+            if(validarCampos.expRegNombre.test(e.target.value)){
+                console.log('buena')
+            }else{
+                console.log('fallo')
+            }
+
+            break;
+        case 'number-card':
+            break;
+        case 'name-bank':
+            break;
+        case 'month-year':
+            break;
+        case 'ccv':
+            break;
+    }
+}
+
+$inputs.forEach(input => {
+    input.addEventListener('keyup', validarFormulario)
+    //input.addEventListener('blur')
+})
 
 const $btnValidar = document.getElementById('btn-validar')
 $btnValidar.addEventListener('click', e => {
     e.preventDefault()
+    
     const numeroTarjeta = document.getElementById('number-card').value
     algoritmoLuhn(numeroTarjeta)
 })
--- js/validarInformacion.js
+++ js/validarInformacion.js
@@ -1,8 +1,6 @@
-const validarCampos = () => {
-    const expRegNombre = `^\w+\s?\w+?\s?$`
-    const expRegNumero = `\d{4}\s\d{4}\s\d{4}\s\d{4}\s`
-    const expRegMMAAAA = `\d{2}[/]\d{4}`
-    const expRegCCV = `\d{3}`
-
-    
+export const validarCampos = {
+    expRegNombre : /^\w+\s?\w+?\s?$/,
+    expRegNumero : /^\d{4}\s\d{4}\s\d{4}\s\d{4}\s/,
+    expRegMMAAAA : /^\d{2}[/]\d{4}/,
+    expRegCCV : /^\d{3}/   
 }
