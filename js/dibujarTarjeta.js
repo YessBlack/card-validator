d312257c4792c68bb8aeede147914f507cfcdd6e
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2022-06-30 22:11:20 -0500

    ajustes

--- js/dibujarTarjeta.js
+++ js/dibujarTarjeta.js
@@ -1,16 +1,16 @@
 export const dibujarTarjeta = (e,input) => {
     switch(e.path[0].id) {
         case 'name-user':
-            document.getElementById('name-user').innerHTML = `${(input.value).toUpperCase()}`   
+            document.getElementById('name-user').textContent = `${(input.value).toUpperCase()}`   
         break;
         case 'number-card-print':
-            document.getElementById('number-card').innerHTML = `${(input.value).toUpperCase()}`   
+            document.getElementById('number-card').textContent = `${(input.value).toUpperCase()}`   
             break;
         case 'name-bank':
-            document.getElementById('name-bank').innerHTML = `${(input.value).toUpperCase()}`   
+            document.getElementById('name-bank').textContent = `${(input.value).toUpperCase()}`   
             break;
         case 'month-year':
-            document.getElementById('month-year').innerHTML = `${(input.value).toUpperCase()}`   
+            document.getElementById('month-year').textContent = `${(input.value).toUpperCase()}`   
             break;
     }
 }
